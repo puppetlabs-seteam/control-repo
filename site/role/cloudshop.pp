57a93320ef858054cba3eee435cb64b216862832
SeanTecha <sean.t@puppet.com>
2017-05-17 15:25:23 -0700

    Fixed comments

--- site/role/cloudshop.pp
+++ site/role/cloudshop.pp
@@ -1,5 +1,5 @@
 # Cloudshop Demo: This demo demonstrates how Puppet can deploy and configure
-# Microsoft SQL Server, IIS, and .NET Framework to stand up a live functioning website.
+# Microsoft SQL Server, IIS, and .NET Framework to stand up a functioning website.
 class role::cloudshop {
   include tse_sqlserver
   include sqlwebapp::db
