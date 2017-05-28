1676414906f2c9d16d712eb5592656f293dacaf4
murdok5 <mike.smith@puppet.com>
2017-05-28 15:38:16 -0700

    update tomcat app

--- site/profile/manifests/app/puppet_tomcat.pp
+++ site/profile/manifests/app/puppet_tomcat.pp
@@ -9,17 +9,17 @@ class profile::app::puppet_tomcat (
     '6': {
       $tomcat_version = '6.0.44'
       $catalina_dir = "/opt/apache-tomcat6"
-      $tomcat_other_versions = [ '7', '8']
+      #$tomcat_other_versions = [ '7', '8']
     }
     '7': {
       $tomcat_version = '7.0.64'
       $catalina_dir = "/opt/apache-tomcat7"
-      $tomcat_other_versions = [ '6', '8']
+      #$tomcat_other_versions = [ '6', '8']
     }
     '8': {
       $tomcat_version = '8.0.26'
       $catalina_dir = "/opt/apache-tomcat8"
-      $tomcat_other_versions = [ '6', '7']
+      #$tomcat_other_versions = [ '6', '7']
     }
   }
 
