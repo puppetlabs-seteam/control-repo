510fa163a50b6bb145b05cfe6922daa9548afdad
murdok5 <mike.smith@puppet.com>
2017-05-28 08:24:30 -0700

    update profiles

--- site/profile/manifests/platform/remote/rhel.pp
+++ site/profile/manifests/platform/remote/rhel.pp
@@ -4,7 +4,7 @@ class profile::platform::remote::rhel {
 
   case $::operatingsystemmajrelease {
     '6': { $openssh_version = '5.3p1-112.el6_7' }
-    '7': { $openssh_version = '6.6.1p1-23.el7_2' }
+    '7': { $openssh_version = '6.6.1p1-22.el7' }
     default: { fail('unsupported operating system') }
   }
 
