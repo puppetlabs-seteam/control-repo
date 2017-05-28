f57739b2df8918132106af5a0e741fe9aa57bbea
murdok5 <mike.smith@puppet.com>
2017-05-28 15:44:35 -0700

    update tomcat app

--- site/profile/manifests/app/puppet_tomcat.pp
+++ site/profile/manifests/app/puppet_tomcat.pp
@@ -5,21 +5,21 @@ class profile::app::puppet_tomcat (
 
   #include profile::firewall
 
-  case $tomcat_major_version {
+  case $::tomcat_major_version {
     '6': {
-      $tomcat_version = '6.0.44'
-      $catalina_dir = "/opt/apache-tomcat6"
-      #$tomcat_other_versions = [ '7', '8']
+      $::tomcat_version = '6.0.44'
+      $::catalina_dir = "/opt/apache-tomcat6"
+      $::tomcat_other_versions = [ '7', '8']
     }
     '7': {
-      $tomcat_version = '7.0.64'
-      $catalina_dir = "/opt/apache-tomcat7"
-      #$tomcat_other_versions = [ '6', '8']
+      $::tomcat_version = '7.0.64'
+      $::catalina_dir = "/opt/apache-tomcat7"
+      $tomcat_other_versions = [ '6', '8']
     }
     '8': {
-      $tomcat_version = '8.0.26'
-      $catalina_dir = "/opt/apache-tomcat8"
-      #$tomcat_other_versions = [ '6', '7']
+      $::tomcat_version = '8.0.26'
+      $::catalina_dir = "/opt/apache-tomcat8"
+      $::tomcat_other_versions = [ '6', '7']
     }
   }
 
--- site/profile/manifests/app/puppet_tomcat/linux.pp
+++ site/profile/manifests/app/puppet_tomcat/linux.pp
@@ -5,7 +5,7 @@ class profile::app::puppet_tomcat::linux {
   }
 
   class { '::tomcat':
-    catalina_home => $catalina_dir,
+    catalina_home => $::catalina_dir,
   }
 
   firewall { '100 allow tomcat access':
@@ -14,25 +14,25 @@ class profile::app::puppet_tomcat::linux {
     action => accept,
   }
 
-  tomcat::instance{ "tomcat${tomcat_major_version}":
+  tomcat::instance{ "tomcat${::tomcat_major_version}":
     install_from_source    => true,
-    source_url             => "http://${::puppet_server}:81/tomcat/apache-tomcat-${tomcat_version}.tar.gz",
+    source_url             => "http://${::puppet_server}:81/tomcat/apache-tomcat-${::tomcat_version}.tar.gz",
     source_strip_first_dir => true,
-    catalina_base          => "${catalina_dir}",
-    catalina_home          => "${catalina_dir}",
-    before                 => Tomcat::War["plsample-${plsample_version}.war"],
+    catalina_base          => "${::catalina_dir}",
+    catalina_home          => "${::catalina_dir}",
+    before                 => Tomcat::War["plsample-${::plsample_version}.war"],
   }
 
-  tomcat::war { "plsample-${plsample_version}.war" :
-    war_source    => "http://${::puppet_server}:81/tomcat/plsample-${plsample_version}.war",
-    catalina_base => "${catalina_dir}",
-    notify        => File["${catalina_dir}/webapps/plsample"],
+  tomcat::war { "plsample-${::plsample_version}.war" :
+    war_source    => "http://${::puppet_server}:81/tomcat/plsample-${::plsample_version}.war",
+    catalina_base => "${::catalina_dir}",
+    notify        => File["${::catalina_dir}/webapps/plsample"],
   }
 
   file { "${catalina_dir}/webapps/plsample":
     ensure => 'link',
-    target => "${catalina_dir}/webapps/plsample-${plsample_version}",
-    notify => Tomcat::Service["plsample-tomcat${tomcat_major_version}"],
+    target => "${::catalina_dir}/webapps/plsample-${::plsample_version}",
+    notify => Tomcat::Service["plsample-tomcat${::tomcat_major_version}"],
   }
 
 #  $tomcat_other_versions.each |String $version| {
@@ -50,11 +50,11 @@ class profile::app::puppet_tomcat::linux {
 #    }
 #  }
 
-  tomcat::service { "plsample-tomcat${tomcat_major_version}":
-    catalina_base => "${catalina_dir}",
-    catalina_home => "${catalina_dir}",
+  tomcat::service { "plsample-tomcat${::tomcat_major_version}":
+    catalina_base => "${::catalina_dir}",
+    catalina_home => "${::catalina_dir}",
     service_name  => "plsample",
-    subscribe     => Tomcat::War["plsample-${plsample_version}.war"],
+    subscribe     => Tomcat::War["plsample-${::plsample_version}.war"],
   }
 
 }
