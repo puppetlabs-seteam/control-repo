bb78502db1138726faa3a78e5e475f588699dbe7
murdok5 <mike.smith@puppet.com>
2017-06-01 10:35:06 -0700

    redo the structure work

--- site/profile/manifests/platform/windows.pp
+++ site/profile/manifests/platform/windows.pp
@@ -1,6 +1,6 @@
 class profile::platform::windows {
 
-  include profile::platform::users::windows_users
+  include profile::platform::users::windows
   include profile::platform::general::windows_regkeys
   include profile::platform::packages::windows
   include profile::platform::firewall::windows
