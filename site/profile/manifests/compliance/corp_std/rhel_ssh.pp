66333f6ba0f79528a536f6bcb89c5624c25719bc
murdok5 <mike.smith@puppet.com>
2017-06-01 11:12:57 -0700

    redo the structure work

---
+++ site/profile/manifests/compliance/corp_std/rhel_ssh.pp
@@ -0,0 +1,22 @@
+class profile::compliance::corp_std::rhel_ssh (
+  $root = 'yes',
+  ){
+
+  file { '/etc/issue':
+    ensure => file,
+    content => template('profile/openssh/banner.erb'),
+  }
+
+  # The $root parameter is being passed to the template to enable toggling of root logins
+  file { '/etc/ssh/sshd_config':
+    ensure => file,
+    content => template('profile/openssh/sshd_config.erb'),
+  }
+
+  service { 'sshd':
+    ensure    => 'running',
+    enable    => true,
+    subscribe => File['/etc/ssh/sshd_config'],
+  }
+
+}
