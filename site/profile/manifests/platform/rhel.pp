67fef33abee77d5fc44f8d9aab52f44cf24722b7
murdok5 <mike.smith@puppet.com>
2017-06-01 12:19:18 -0700

    redo the structure work

--- site/profile/manifests/platform/rhel.pp
+++ site/profile/manifests/platform/rhel.pp
@@ -2,6 +2,6 @@ class profile::platform::rhel {
 
   include profile::platform::remote::rhel
   include profile::platform::software::rhel
-  include profile::platform::general::rhel_users
+  include profile::platform::users::rhel
 
 }
