6d271d97809bb44bac27b744b8d85c2b2fae6ff0
Tommy Speigner <tspeigner@gmail.com>
2017-06-15 15:30:45 -0700

    updated readme.md

--- site/profile/manifests/app/splunk/README.md
+++ site/profile/manifests/app/splunk/README.md
@@ -1,1 +1,2 @@
 # Sets up Splunk Server
+# Specify settings in the module to start with, with plans to move it to hiera
---
+++ site/profile/manifests/app/splunk/splunk.pp
@@ -0,0 +1,32 @@
+class profile::splunk (
+  $purge_inputs    = false,
+  $purge_outputs   = false,
+  $src_server      = 'master.inf.puppet.vm',
+  $src_ip          = '',
+  $src_path        = 'splunk/files',
+  $version         = '6.6.1',
+  $build           = 'aeae3fe0c5af',
+) {
+
+  #$src_root       = "http://$src_server:81/$src_path"
+
+# Sets the source server host/ip information on the node.
+  host { $src_server:
+    ip => $src_ip,
+  }
+
+  class { 'splunk::params':
+    version              => $version,
+    build                => $build,
+    server               => $server,
+  }
+
+  class { 'splunk':
+    logging_port  => $logging_port,
+    splunkd_port  => $splunkd_port,
+    web_port      => $web_port,
+    purge_inputs  => $purge_inputs,
+    purge_outputs => $purge_outputs,
+  }
+
+}
