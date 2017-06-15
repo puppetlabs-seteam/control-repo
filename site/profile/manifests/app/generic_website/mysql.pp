62d178a1537f6d31a4e8b6c0992e2ef6089b9fd8
Grace Andrews <grace.e.andrews@gmail.com>
2017-06-15 13:14:47 -0700

    Create mysql.pp

--- site/profile/manifests/app/generic_website/mysql.pp
+++ site/profile/manifests/app/generic_website/mysql.pp
@@ -1,5 +1,5 @@
 # @summary This profile installs mysql without default accounts
-class profile::generic_webdemo::mysql {
+class profile::sample_website::mysql {
 
   class { 'mysql::server':
     remove_default_accounts => true
