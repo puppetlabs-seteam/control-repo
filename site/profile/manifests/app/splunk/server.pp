3ba2ed1f6cf8b2905fc427b9d3f605548a9c1c5b
Tommy Speigner <tspeigner@gmail.com>
2017-06-15 19:50:29 -0700

    added file name to dir

--- site/profile/manifests/app/splunk/server.pp
+++ site/profile/manifests/app/splunk/server.pp
@@ -37,7 +37,7 @@ class profile::app::splunk::server (
   
   file { 'splunk-6.6.1-aeae3fe0c5af-linux-2.6-x86_64.rpm' :
     ensure           => file,
-    path             => '/opt/tse-files/splunk/files/linux/',
+    path             => '/opt/tse-files/splunk/files/linux/splunk-6.6.1-aeae3fe0c5af-linux-2.6-x86_64.rpm',
     force            => true,
     source           => $package_source,
   }
