12be5ed961fe4a66fd22a5d6032f7814fb6d97ee
Tommy Speigner <tspeigner@gmail.com>
2017-06-15 19:30:14 -0700

    restructred package_source and removed user and group info

--- site/profile/manifests/app/splunk/server.pp
+++ site/profile/manifests/app/splunk/server.pp
@@ -1,13 +1,13 @@
 # splunk server module
 class profile::app::splunk::server (
-  $purge_inputs    = false,
-  $purge_outputs   = false,
-  #$src_server      = 'master.inf.puppet.vm',
-  #$src_ip          = '',
-  #$src_path        = 'splunk/files',
-  $version         = '6.6.1',
-  $build           = 'aeae3fe0c5af',
-  #$src_root        = "https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=6.6.1&product=universalforwarder&filename=splunkforwarder-6.6.1-aeae3fe0c5af-linux-2.6-x86_64.rpm&wget=true",
+  $purge_inputs       = false,
+  $purge_outputs      = false,
+  #$src_server        = 'master.inf.puppet.vm',
+  #$src_ip            = '',
+  #$src_path          = 'splunk/files',
+  $version            = '6.6.1',
+  $build              = 'aeae3fe0c5af',
+  $package_source     = "https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=6.6.1&product=universalforwarder&filename=splunkforwarder-6.6.1-aeae3fe0c5af-linux-2.6-x86_64.rpm&wget=true",
 ) {
 
 
@@ -37,9 +37,7 @@ class profile::app::splunk::server (
   
   file { "/opt/tse-files/splunk/files/linux/splunk-6.6.1-aeae3fe0c5af-linux-2.6-x86_64.rpm":
     ensure           => present,
-    owner            => 'pe-puppet',
-    group            => 'pe-puppet',
-    source           => "https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=6.6.1&product=universalforwarder&filename=splunkforwarder-6.6.1-aeae3fe0c5af-linux-2.6-x86_64.rpm&wget=true",
+    source           => $package_source,
   }
 
 
