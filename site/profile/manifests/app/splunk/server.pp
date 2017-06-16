e626aa9997729ad6bce56adfd9911d8d965df0fc
Tommy Speigner <tspeigner@gmail.com>
2017-06-15 22:28:09 -0700

    added params and removed other content

--- site/profile/manifests/app/splunk/server.pp
+++ site/profile/manifests/app/splunk/server.pp
@@ -2,28 +2,20 @@
 class profile::app::splunk::server (
   $purge_inputs       = false,
   $purge_outputs      = false,
-  #$src_server        = 'master.inf.puppet.vm',
-  #$src_ip            = '',
-  #$src_path          = 'splunk/files',
   $version            = '6.6.1',
   $build              = 'aeae3fe0c5af',
-  $package_source     = "https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=6.6.1&product=universalforwarder&filename=splunkforwarder-6.6.1-aeae3fe0c5af-linux-2.6-x86_64.rpm&wget=true",
+  $file_host          = 'master.inf.puppet.vm',
+  $file_path          = '/opt/tse-files/splunk/files/splunk/linux',
+  $server             = 'monitor.inf.puppet.vm',
 ) {
 
-
-# Sets the source server host/ip information on the node.
-#  host { $src_server:
-#    ip => $src_ip,
-#  }
+  $src_root = http://$file_host:81/$file_path,
 
   class { 'splunk::params':
     version  => $version,
     build    => $build,
     server   => $server,
     src_root => $src_root,
-  }
-
-  class { 'splunk':
     logging_port  => $logging_port,
     splunkd_port  => $splunkd_port,
     web_port      => $web_port,
@@ -31,16 +23,8 @@ class profile::app::splunk::server (
     purge_outputs => $purge_outputs,
   }
 
- # file { [ '/opt/tse-files', '/opt/tse-files/splunk', '/opt/tse-files/splunk/files', '/opt/tse-files/splunk/files/linux'] :
- #   ensure           => directory,
- # }
-  
-  file { 'splunk-6.6.1-aeae3fe0c5af-linux-2.6-x86_64.rpm' :
-    ensure           => file,
-    path             => '/opt/tse-files/splunk/files/linux/splunk-6.6.1-aeae3fe0c5af-linux-2.6-x86_64.rpm',
-    force            => true,
-    source           => $package_source,
+#  class { 'splunk':
+#  }
+#
+
   }
-
-
-}
