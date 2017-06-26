9451a9f0975ed93fac5c4486bd50f2e991c3c49c
Tommy Speigner <tspeigner@gmail.com>
2017-06-26 14:31:08 -0700

    making things look nice with abirs help

--- site/profile/manifests/platform/monitoring/splunkforwarder.pp
+++ site/profile/manifests/platform/monitoring/splunkforwarder.pp
@@ -1,19 +1,19 @@
 # Splunk Forwarder class
 class profile::platform::monitoring::splunkforwarder (
-  $splunk_server          = 'ip-10-98-10-78.us-west-2.compute.internal',
+  $splunk_server,
 ){
+
+if $splunk_server == undef {
+  $splunk_nodes_query = 'resources[certname] {type = "Class" and title = "Splunk" }'
+  $splunk_server = puppetdb_query($splunk_nodes_query)[0][certname]
+  notify {'$splunk_server':}
+  }
+
 
   class { '::splunk::params':
     server   => $splunk_server,
     src_root => 'http://tseteam.s3.amazonaws.com/files',
-}
+  }
 
 include ::splunk::forwarder
-
-
-    $splunk_nodes_query = 'resources[certname] { type = "Class" and title = "Splunk" }'
-    $splunk_nodes = puppetdb_query($splunk_nodes_query)[0][certname]
-
-    notify {"$splunk_nodes":}
-
 }
