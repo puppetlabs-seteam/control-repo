4db47a881222b6179a0ccc22d2ed34e3cceba50d
Tommy Speigner <tspeigner@gmail.com>
2017-06-26 15:39:22 -0700

    fixing things, hopefully

--- site/profile/manifests/platform/monitoring/splunkforwarder.pp
+++ site/profile/manifests/platform/monitoring/splunkforwarder.pp
@@ -2,14 +2,18 @@
 class profile::platform::monitoring::splunkforwarder (
   $splunk_server,
 ){
-if $splunk_server==undef {
-  $splunk_nodes_query = 'resources[certname] { type = "Class" and title = "Splunk" }'
+
+if $splunk_server == undef {
+  $splunk_nodes_query = 'resources[certname] {type = "Class" and title = "Splunk" }'
   $splunk_server = puppetdb_query($splunk_nodes_query)[0][certname]
-  notify {"$splunk_nodes":}
-}
+  #notify { '$splunk_server': }
+  }
+
+
   class { '::splunk::params':
     server   => $splunk_server,
     src_root => 'http://tseteam.s3.amazonaws.com/files',
-}
+  }
+
 include ::splunk::forwarder
 }
