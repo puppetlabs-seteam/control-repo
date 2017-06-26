2cae6e0d7c96ac26a8cc7a0b67adac3208602057
Tommy Speigner <tspeigner@gmail.com>
2017-06-26 13:42:40 -0700

    update with puppetdb_query

--- site/profile/manifests/platform/monitoring/splunkforwarder.pp
+++ site/profile/manifests/platform/monitoring/splunkforwarder.pp
@@ -1,19 +1,18 @@
 class profile::platform::monitoring::splunkforwarder (
-  $splunk_server          = 'ip-10-98-10-78.us-west-2.compute.internal',
+  #$splunk_server          = 'ip-10-98-10-78.us-west-2.compute.internal',
+  $splunk_nodes_query = 'resources[certname] { type = "Class" and title = "Splunk" }'
+  $splunk_nodes = puppetdb_query($splunk_nodes_query)[0][certname]
+
 ){
 
   class { '::splunk::params':
-    server           => $splunk_server,
-    src_root         => "http://tseteam.s3.amazonaws.com/files",
+    server   => $splunk_server,
+    src_root => 'http://tseteam.s3.amazonaws.com/files',
 }
 
 include ::splunk::forwarder
 
 
-#  $splunk_srv     = Splunk_server <<||>>
+notify {"$splunk_nodes":}
 
-#notify{"The value is: ${splunk_srv}": }
-#notify { 'Splunk_server <<||>>': }
- 
-  
 }
