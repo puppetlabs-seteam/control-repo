4cb84065b4e2323b2e62ad5c5614eb89302f2d8a
ipcrm <mcadorette@gmail.com>
2017-08-10 09:55:33 -0400

    Split Java/Entropy profiles,remove inheritance

--- site/profile/manifests/app/puppet_tomcat/linux.pp
+++ site/profile/manifests/app/puppet_tomcat/linux.pp
@@ -1,14 +1,18 @@
-class profile::app::puppet_tomcat::linux inherits profile::app::puppet_tomcat {
+class profile::app::puppet_tomcat::linux (
+  String $plsample_version,
+  String $tomcat_version,
+  String $catalina_dir,
+  Array  $tomcat_other_versions,
+) {
 
-  # tomcat starting takes 2-10 minutes currently due to /dev/random seed of entorpy. VM doesnt have much work
-  # hence no entropy.
+  include ::profile::app::entropy
 
-  class { 'java':
-    distribution => 'jre'
+  class {'::profile::app::java':
+    distribution => 'jre',
   }
 
   class { '::tomcat':
-    catalina_home => $::profile::app::puppet_tomcat::catalina_dir,
+    catalina_home => $catalina_dir,
     before        => Class['java'],
   }
 
@@ -18,30 +22,30 @@ class profile::app::puppet_tomcat::linux inherits profile::app::puppet_tomcat {
     action => accept,
   }
 
-  tomcat::instance{ "tomcat${profile::app::puppet_tomcat::tomcat_major_version}":
+  tomcat::instance{ "tomcat${tomcat_version}":
     install_from_source    => true,
     #source_url             => "https://s3.amazonaws.com/saleseng/files/tomcat/apache-tomcat-8.0.44.tar.gz",
-    source_url             => "http://${::puppet_server}:81/tomcat/apache-tomcat-${::profile::app::puppet_tomcat::tomcat_version}.tar.gz",
+    source_url             => "http://${::puppet_server}:81/tomcat/apache-tomcat-${tomcat_version}.tar.gz",
     source_strip_first_dir => true,
-    catalina_base          => $::profile::app::puppet_tomcat::catalina_dir,
-    catalina_home          => $::profile::app::puppet_tomcat::catalina_dir,
-    before                 => Tomcat::War["plsample-${::profile::app::puppet_tomcat::plsample_version}.war"],
+    catalina_base          => $catalina_dir,
+    catalina_home          => $catalina_dir,
+    before                 => Tomcat::War["plsample-${plsample_version}.war"],
   }
 
-  tomcat::war { "plsample-${::profile::app::puppet_tomcat::plsample_version}.war" :
+  tomcat::war { "plsample-${plsample_version}.war" :
     #war_source    => "https://s3.amazonaws.com/saleseng/files/tomcat/sample-1.0.war",
-    war_source    => "http://${::puppet_server}:81/tomcat/plsample-${::profile::app::puppet_tomcat::plsample_version}.war",
-    catalina_base => $::profile::app::puppet_tomcat::catalina_dir,
-    notify        => File["${::profile::app::puppet_tomcat::catalina_dir}/webapps/plsample"],
+    war_source    => "http://${::puppet_server}:81/tomcat/plsample-${plsample_version}.war",
+    catalina_base => $catalina_dir,
+    notify        => File["${catalina_dir}/webapps/plsample"],
   }
 
-  file { "${::profile::app::puppet_tomcat::catalina_dir}/webapps/plsample":
+  file { "${catalina_dir}/webapps/plsample":
     ensure => 'link',
-    target => "${::profile::app::puppet_tomcat::catalina_dir}/webapps/plsample-${::profile::app::puppet_tomcat::plsample_version}",
-    notify => Tomcat::Service["plsample-tomcat${::profile::app::puppet_tomcat::tomcat_major_version}"],
+    target => "${catalina_dir}/webapps/plsample-${plsample_version}",
+    notify => Tomcat::Service["plsample-tomcat${tomcat_version}"],
   }
 
-  $::profile::app::puppet_tomcat::tomcat_other_versions.each |String $version| {
+  $tomcat_other_versions.each |String $version| {
     service {"tomcat-plsample-tomcat${version}":
       ensure => stopped,
       status => "ps aux | grep \'catalina.base=/opt/apache-tomcat${version}\' | grep -v grep",
@@ -52,15 +56,15 @@ class profile::app::puppet_tomcat::linux inherits profile::app::puppet_tomcat {
       ensure => absent,
       force  => true,
       backup => false,
-      before => Tomcat::Service["plsample-tomcat${::profile::app::puppet_tomcat::tomcat_major_version}"],
+      before => Tomcat::Service["plsample-tomcat${tomcat_version}"],
     }
   }
 
-  tomcat::service { "plsample-tomcat${::profile::app::puppet_tomcat::tomcat_major_version}":
-    catalina_base => $::profile::app::puppet_tomcat::catalina_dir,
-    catalina_home => $::profile::app::puppet_tomcat::catalina_dir,
+  tomcat::service { "plsample-tomcat${tomcat_version}":
+    catalina_base => $catalina_dir,
+    catalina_home => $catalina_dir,
     service_name  => 'plsample',
-    subscribe     => Tomcat::War["plsample-${::profile::app::puppet_tomcat::plsample_version}.war"],
+    subscribe     => Tomcat::War["plsample-${plsample_version}.war"],
   }
 
 }
