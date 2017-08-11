5e8bd27739e74795ef324d8ca2f912c1ef542c51
ipcrm <mcadorette@gmail.com>
2017-08-11 18:58:31 -0400

    remove missing profile from windows baseline

--- site/profile/manifests/platform/baseline/windows.pp
+++ site/profile/manifests/platform/baseline/windows.pp
@@ -4,7 +4,6 @@ class profile::platform::baseline::windows {
   include ::profile::platform::baseline::windows::motd
   include ::profile::platform::baseline::windows::firewall
   include ::profile::platform::baseline::windows::packages
-  include ::profile::platform::baseline::windows::powershell
   include ::profile::platform::baseline::users::windows
 
 }
