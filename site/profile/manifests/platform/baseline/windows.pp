4a242567e957189a93e7efd7b55b47803830bd92
ipcrm <mcadorette@gmail.com>
2017-08-11 17:55:23 -0400

    manage windows firewall

--- site/profile/manifests/platform/baseline/windows.pp
+++ site/profile/manifests/platform/baseline/windows.pp
@@ -2,6 +2,7 @@ class profile::platform::baseline::windows {
 
   include ::profile::platform::baseline::windows::common
   include ::profile::platform::baseline::windows::motd
+  include ::profile::platform::baseline::windows::firewall
   include ::profile::platform::baseline::windows::packages
   include ::profile::platform::baseline::windows::powershell
   include ::profile::platform::baseline::users::windows
