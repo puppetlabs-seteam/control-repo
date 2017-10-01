eb423bca16fe3def6f0b4e16405f1248636dd55a
ipcrm <mcadorette@gmail.com>
2017-10-01 08:55:37 -0400

    test

--- site/profile/manifests/app/jenkins/slave.pp
+++ site/profile/manifests/app/jenkins/slave.pp
@@ -8,10 +8,11 @@ class profile::app::jenkins::slave (
 
     'Linux': {
       class { '::jenkins::slave':
-        masterurl => $master_url,
-        ui_user   => 'admin',
-        ui_pass   => 'password',
-        labels    => ['tse-slave-linux','tse-control-repo'],
+        masterurl    => $master_url,
+        ui_user      => 'admin',
+        ui_pass      => 'password',
+        labels       => ['tse-slave-linux','tse-control-repo'],
+        slave_groups => 'wheel',
       }
 
       include ::profile::app::rubydev
