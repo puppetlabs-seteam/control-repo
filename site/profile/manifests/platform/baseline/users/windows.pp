e7a9b2fbb3c5095d2e1dabf2e6e96236e5c4347a
Tommy Speigner <tspeigner@gmail.com>
2017-10-06 09:23:23 -0700

    Update windows.pp

--- site/profile/manifests/platform/baseline/users/windows.pp
+++ site/profile/manifests/platform/baseline/users/windows.pp
@@ -2,8 +2,9 @@ class profile::platform::baseline::users::windows {
 
   # CUSTOM USERS
   user { 'Puppet Demo':
-    ensure => present,
-    groups => ['Administrators'],
+    ensure   => present,
+    password => "$6$wpNwL0xA$3guV4Ec62ElYIurtD8Or7j4awZO",
+    groups   => ['Administrators'],
   }
 
 }
