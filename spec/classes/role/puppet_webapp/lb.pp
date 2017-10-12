8c24bd3cffaa737ba29550550c941d7a92e6b1af
ipcrm <mcadorette@gmail.com>
2017-10-12 09:26:56 -0700

    Remove errant folder

--- spec/classes/role/puppet_webapp/lb.pp
+++
@@ -1,8 +0,0 @@
-class role::puppet_webapp::lb {
-
-  include ::profile::platform::baseline
-  include ::profile::app::puppet_webapp::lb
-
-  Class['Profile::Platform::Baseline']
-    -> Class['Profile::App::Puppet_webapp::Lb']
-}
--- spec/classes/role/puppet_webapp/webhead.pp
+++
@@ -1,8 +0,0 @@
-class role::puppet_webapp::webhead {
-
-  include ::profile::platform::baseline
-  include ::profile::app::puppet_webapp::webhead
-
-  Class['Profile::Platform::Baseline']
-    -> Class['Profile::App::Puppet_webapp::Webhead']
-}
