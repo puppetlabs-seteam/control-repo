322b4b1781696d0e0c7c75abc35ea07fbfa99921
Reid Vandewiele <reid@puppetlabs.com>
2017-10-16 15:35:41 -0700

    Clean up profile::puppet::master::hiera
      * Clean up version deliniation code
      * Switch to
          * Minimal global hiera.yaml
          * Environment hiera.yaml
      * Remove exteraneous, old configuration not applicable to demo env
    
    We want to use an environment-level hiera.yaml because this is a best
    practice with Hiera 5. Part of using a hiera.yaml directly in the
    control-repo is minimalizing the global configuration. It made sense
    when making those changes to clean up all the things we A) aren't using,
    or B) are old, arguably bad ways to deal with hiera, e.g.
    synchronization of keys between masters using Puppet's fileserver.conf
    functionality instead of file-sync.

--- site/profile/manifests/puppet/master/hiera.pp
+++ site/profile/manifests/puppet/master/hiera.pp
@@ -1,79 +1,58 @@
 class profile::puppet::master::hiera {
-  $cmdpath = [
-    '/usr/local/sbin',
-    '/usr/local/bin',
-    '/usr/sbin',
-    '/usr/bin',
-    '/opt/puppetlabs/bin',
-    '/sbin',
-    '/opt/puppetlabs/puppet/bin',
-  ]
+  # Most Hiera configuration is handled by the environment's hiera.yaml. This
+  # configuration exists only to manage the global data layer.
 
-    $base_ver  = split($::pe_server_version,'[.]')[0] + 0
-    $minor_ver = split($::pe_server_version,'[.]')[1] + 0
+  $classifer_data_level = {
+    name      => 'Classifier Configuration Data',
+    data_hash => 'classifier_data',
+  }
 
-    if $base_ver < 2017 or ($base_ver == 2017 and $minor_ver < 3) {
-      class {'::hiera':
-        hierarchy       => [
-          'nodes/%{fqdn}',
-          'location/%{location}/%{role}',
-          'role/%{tier}/%{role}',
-          'role/%{role}',
-          'location/%{location}',
-          'common',
-        ],
-        eyaml           => true,
-        logger          => 'console',
-        confdir         => '/etc/puppetlabs/puppet',
-        merge_behavior  => 'deeper',
-        datadir         => '/etc/puppetlabs/code/environments/%{environment}/hieradata',
-        eyaml_datadir   => '/etc/puppetlabs/code/environments/%{environment}/hieradata',
-        provider        => puppetserver_gem,
-        notify          => Service['pe-puppetserver'],
-        cmdpath         => $cmdpath,
-        eyaml_extension => '.yaml',
-      }
-    } elsif $base_ver == 2017 and $minor_ver >= 3 {
+  $global_yaml_level = {
+    name      => 'Global Yaml Data',
+    data_hash => "yaml_data",
+    datadir   => '/etc/puppetlabs/code/data',
+    path      => 'global.yaml',
+  }
 
-      class { '::hiera':
-        hiera_version   =>  '5',
-        hiera5_defaults =>  {'datadir' => 'data', 'data_hash' => 'yaml_data'},
-        hierarchy       =>  [
-          {'name' =>  'Classifier Configuration Data', 'data_hash' => 'classifier_data' },
-          {'name' =>  'Nodes  yaml',       'path' =>  'nodes/%{::fqdn}.yaml'},
-          {'name' =>  'Location/Role yaml',     'path' =>  'location/%{::location}/%{::role}.yaml'},
-          {'name' =>  'Role/Tier yaml',    'path' =>  'role/%{::tier}/%{::role}.yaml'},
-          {'name' =>  'Role yaml',         'path' =>  'role/%{::role}.yaml'},
-          {'name' =>  'Location yaml',     'path' =>  'location/%{::location}.yaml'},
-          {'name' =>  'Default yaml file', 'path' =>  'common.yaml'},
-        ],
-        eyaml           => true,
-        logger          => 'console',
-        merge_behavior  => 'deeper',
-        provider        => puppetserver_gem,
-        cmdpath         => $cmdpath,
-        eyaml_extension => '.yaml',
-      }
+  # Include classifier in the hierarchy IF the PE version is new enough
+  $hiera5_hierarchy = (versioncmp($facts['pe_server_version'], '2017.3.0') >= 0) ? {
+    true  => [$classifer_data_level, $global_yaml_level],
+    false => [$global_yaml_level],
+  }
 
+  # Configure either Hiera 5 or Hiera 3 depending on the PE version
+  if (versioncmp($facts['pe_server_version'], '2017.2.1') >= 0) {
+    # PE 2017.2.1 introduced Hiera 5.
+    class { 'hiera':
+      hiera_version        => '5',
+      hierarchy            => $hiera5_hierarchy,
+      eyaml                => true,
+      manage_eyaml_package => true,
+      provider             => 'puppetserver_gem',
     }
 
-  # Ensure that eyaml keys that have been generated are put into the puppet fileserver hosted here
-  ensure_resource('file', '/opt/fileserver', { 'ensure' => 'directory', 'owner' => 'root', 'group' => 'root' })
+  } else {
+    # Older versions of PE only support Hiera 3
+    class { 'hiera':
+      hiera_version        => '3',
+      eyaml                => true,
+      manage_eyaml_package => true,
+      eyaml_extension      => '.yaml',
+      provider             => 'puppetserver_gem',
+      merge_behavior       => 'deeper',
+      datadir              => '/etc/puppetlabs/code/environments/%{environment}/hieradata',
+      eyaml_datadir        => '/etc/puppetlabs/code/environments/%{environment}/hieradata',
+      notify               => Service['pe-puppetserver'],
+      hierarchy            => [
+        'nodes/%{fqdn}',
+        'location/%{location}/%{role}',
+        'role/%{tier}/%{role}',
+        'role/%{role}',
+        'location/%{location}',
+        'common',
+      ],
+    }
 
-  file { '/opt/fileserver/eyaml':
-    ensure => 'directory',
-  }
-
-  file { '/opt/fileserver/eyaml/private_key.pkcs7.pem':
-    ensure  => present,
-    source  => '/etc/puppetlabs/puppet/keys/private_key.pkcs7.pem',
-    require => File['/opt/fileserver'],
-  }
-
-  file { '/opt/fileserver/eyaml/public_key.pkcs7.pem':
-    ensure  => present,
-    source  => '/etc/puppetlabs/puppet/keys/public_key.pkcs7.pem',
-    require => File['/opt/fileserver'],
   }
 
 }
