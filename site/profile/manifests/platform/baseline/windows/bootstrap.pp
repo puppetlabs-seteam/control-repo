dfe1e1aa3bbeaa4effbe7ae266a4f41766bbe5e0
murdok5 <mike.smith@puppet.com>
2017-10-18 12:23:10 -0700

    2016 edits and testing

--- site/profile/manifests/platform/baseline/windows/bootstrap.pp
+++ site/profile/manifests/platform/baseline/windows/bootstrap.pp
@@ -2,10 +2,24 @@ class profile::platform::baseline::windows::bootstrap {
 
   require ::chocolatey
 
+  # service needs to be running to install the update
+  service { 'wuauserv':
+    ensure  => 'running',
+    enable => 'true',
+  }
+
+  # disable auto updating so machine doesnt start downloading / updating
+  registry::value { 'disable updates':
+    key   => 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU',
+    value => 'NoAutoUpdate',
+    data  => "1",
+    type  => 'dword',
+  }
+
   # Get WMF
   package { 'powershell':
     ensure          => latest,
-    install_options => ['-pre','--ignore-package-exit-codes'],
+    #install_options => ['-pre','--ignore-package-exit-codes'],
     provider        => chocolatey,
   }
 
--- site/role/manifests/cloudshop.pp
+++ site/role/manifests/cloudshop.pp
@@ -1,6 +1,14 @@
 # Cloudshop Demo: This demo demonstrates how Puppet can deploy and configure
 # Microsoft SQL Server, IIS, and .NET Framework to stand up a functioning website.
 class role::cloudshop {
-  include ::profile::platform::baseline
-  include ::profile::app::cloudshop
+
+  case $::operatingsystemmajrelease {
+    '2012 R2':{
+      include ::profile::platform::baseline
+      include ::profile::app::cloudshop
+    }
+    default:{
+      fail('Unsupported OS')
+    }
+  }
 }
