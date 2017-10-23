e798ceb3adcb2e75adf8ba59eefa8c0507c64902
murdok5 <mike.smith@puppet.com>
2017-10-23 07:44:43 -0700

    Resolve build linting errors

--- site/profile/manifests/platform/baseline/windows/bootstrap.pp
+++ site/profile/manifests/platform/baseline/windows/bootstrap.pp
@@ -2,28 +2,28 @@ class profile::platform::baseline::windows::bootstrap {
 
   require ::chocolatey
 
-  if versioncmp($powershell_version, "5.1.0") <= 0 {
+  if versioncmp($::powershell_version, '5.1.0') <= 0 {
     #notify{"version was less than":}
 
     # service needs to be running to install the update
     service { 'wuauserv':
-      ensure  => 'running',
-      enable => 'true',
+      ensure => 'running',
+      enable => true,
     }
 
     # disable auto updating so machine doesnt start downloading / updating
     registry::value { 'disable updates':
       key   => 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU',
       value => 'NoAutoUpdate',
-      data  => "1",
+      data  => '1',
       type  => 'dword',
     }
 
     # Get WMF
     package { 'powershell':
-      ensure          => latest,
+      ensure   => latest,
       #install_options => ['-pre','--ignore-package-exit-codes'],
-      provider        => chocolatey,
+      provider => chocolatey,
     }
 
     reboot {'dsc_install':
