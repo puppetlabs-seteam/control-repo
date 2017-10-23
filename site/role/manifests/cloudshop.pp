98dd983321c6b3fc687600ae92ad89f3fe28a5d1
ipcrm <mcadorette@gmail.com>
2017-10-23 19:28:55 -0400

    Remove case logic from cloudshop role

--- site/role/manifests/cloudshop.pp
+++ site/role/manifests/cloudshop.pp
@@ -1,14 +1,6 @@
 # Cloudshop Demo: This demo demonstrates how Puppet can deploy and configure
 # Microsoft SQL Server, IIS, and .NET Framework to stand up a functioning website.
 class role::cloudshop {
-
-  case $::operatingsystemmajrelease {
-    '2012 R2':{
-      include ::profile::platform::baseline
-      include ::profile::app::cloudshop
-    }
-    default:{
-      fail('Unsupported OS')
-    }
-  }
+  include ::profile::platform::baseline
+  include ::profile::app::cloudshop
 }
