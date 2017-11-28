638af8080021bfc28096d274aee297bbd2c7471b
ipcrm <mcadorette@gmail.com>
2017-11-27 22:21:25 -0500

    Fixing Lint issues

--- site/profile/manifests/app/cloudshop.pp
+++ site/profile/manifests/app/cloudshop.pp
@@ -2,7 +2,7 @@ class profile::app::cloudshop {
 
   case $::kernel {
     'windows': {
-      if $::iis_version == "8.5" {
+      if $::iis_version == '8.5' {
         include profile::app::cloudshop::sqlserver::init
         include profile::app::cloudshop::webapp::db
         include profile::app::cloudshop::webapp::init
