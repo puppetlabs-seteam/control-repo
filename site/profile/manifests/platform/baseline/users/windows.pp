d3b0869dde4037b524280638337375c8de644626
Tommy Speigner <tspeigner@gmail.com>
2018-02-21 09:18:54 -0800

    fixing bracket issue

--- site/profile/manifests/platform/baseline/users/windows.pp
+++ site/profile/manifests/platform/baseline/users/windows.pp
@@ -4,6 +4,7 @@ class profile::platform::baseline::users::windows {
   user { 'Puppet Demo':
     ensure   => present,
     password => '$6$wpNwL0xA$3guV4Ec62ElYIurtD8Or7j4awZO',
+  }
   user { 'Sample Demo':
     ensure   => present,
     password => 'Puppet4Life!17',
