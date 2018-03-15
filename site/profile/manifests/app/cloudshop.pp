b9cac8c44b8092af1dcb9ed99e3852aa88cdd4c7
puppet <puppet@localhost.local>
2018-03-15 16:56:16 +0000

    removing iis requirement

--- site/profile/manifests/app/cloudshop.pp
+++ site/profile/manifests/app/cloudshop.pp
@@ -2,11 +2,9 @@ class profile::app::cloudshop {
 
   case $::kernel {
     'windows': {
-      if $::iis_version == '8.5' {
         include profile::app::cloudshop::sqlserver::init
         include profile::app::cloudshop::webapp::db
         include profile::app::cloudshop::webapp::init
-      }
     }
     default: {
       fail('Unsupported OS')
