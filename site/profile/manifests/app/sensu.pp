afd1575a4781553b5bccd0635c8a8047b69e2ba2
maju6406 <beersy@gmail.com>
2018-05-23 12:41:29 -0700

    Fixing uchiwa bug again (hopefully)

--- site/profile/manifests/app/sensu.pp
+++ site/profile/manifests/app/sensu.pp
@@ -4,7 +4,5 @@ class profile::app::sensu {
   include profile::app::sensu::plugins
   include profile::app::sensu::checks
   include profile::app::sensu::handlers
-  class { 'profile::app::sensu::uchiwa':
-    require => Class['::profile::app::sensu::server']
-  }
+  include profile::app::sensu::uchiwa
 }
