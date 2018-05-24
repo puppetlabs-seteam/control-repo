9204f3f53e3001a21cef0b21060c9fa214496955
maju6406 <beersy@gmail.com>
2018-05-24 15:32:00 -0700

    Updating gitea configuration

--- site/profile/manifests/puppet/master/gitea.pp
+++ site/profile/manifests/puppet/master/gitea.pp
@@ -1,5 +1,5 @@
 class profile::puppet::master::gitea {
-
+  $secret_key = 'mysecretkey'
   class { 'gitea':
       package_ensure         => 'present',
       dependencies_ensure    => 'present',
@@ -17,12 +17,64 @@ class profile::puppet::master::gitea {
       repository_root        => '/var/git',
       log_directory          => '/var/log/gitea',
       attachment_directory   => '/opt/gitea/data/attachments',
-      configuration_sections => {},
       manage_service         => true,
       service_template       => 'gitea/systemd.erb',
       service_path           => '/lib/systemd/system/gitea.service',
       service_provider       => 'systemd',
       service_mode           => '0644',
+      configuration_sections => {
+        'server'     => {
+          'DOMAIN'           => $::fqdn,
+          'HTTP_PORT'        => 3000,
+          'ROOT_URL'         => "https://${::fqdn}/",
+          'HTTP_ADDR'        => '0.0.0.0',
+          'DISABLE_SSH'      => false,
+          'SSH_PORT'         => '22',
+          'START_SSH_SERVER' => false,
+          'OFFLINE_MODE'     => false,
+        },
+        'database'   => {
+          'DB_TYPE'  => 'sqlite3',
+          'HOST'     => '127.0.0.1:3306',
+          'NAME'     => 'gitea',
+          'USER'     => 'root',
+          'PASSWD'   => '',
+          'SSL_MODE' => 'disable',
+          'PATH'     => '/opt/gitea/data/gitea.db',
+        },
+        'security'   => {
+          'SECRET_KEY'   => 'thesecretkey',
+          'INSTALL_LOCK' => true,
+        },
+        'service'    => {
+          'REGISTER_EMAIL_CONFIRM' => false,
+          'ENABLE_NOTIFY_MAIL'     => false,
+          'DISABLE_REGISTRATION'   => false,
+          'ENABLE_CAPTCHA'         => true,
+          'REQUIRE_SIGNIN_VIEW'    => false,
+        },
+        'repository' => {
+          'ROOT'     => '/var/git',
+        },
+        'mailer'     => {
+          'ENABLED' => false,
+        },
+        'picture'    => {
+          'DISABLE_GRAVATAR'        => false,
+          'ENABLE_FEDERATED_AVATAR' => true,
+        },
+        'session'    => {
+          'PROVIDER' => 'file',
+        },
+        'log'        => {
+          'MODE'      => 'file',
+          'LEVEL'     => 'info',
+          'ROOT_PATH' => '/opt/gitea/log',
+        },
+        'webhook'    => {
+          'SKIP_TLS_VERIFY' => true,
+        },
+      }   
   }
 
   firewall{ '100 allow web connections':
