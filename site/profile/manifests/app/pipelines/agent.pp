38f3c327fbcbeafc56dabefb0cc80b9bba668cbe
maju6406 <beersy@gmail.com>
2018-12-26 17:50:49 -0500

    Conforming to latest puppet standards

--- site/profile/manifests/app/pipelines/agent.pp
+++ site/profile/manifests/app/pipelines/agent.pp
@@ -4,18 +4,14 @@ class profile::app::pipelines::agent (
   String $download_url,
 ){
 
-#  if ($access_token == 'ENTER_VALUE') or ($access_token == 'fake'){
-#    fail('You must enter access_token, secret_key, and download_url if you want to install the Pipelines agent.')
-#  }
-
-  class { '::pipelines::agent':
+  class { 'pipelines::agent':
     access_token => Sensitive($access_token),
     secret_key   => Sensitive($secret_key),
     download_url => $download_url,
     start_agent  => true,
   }
 
-  case $::kernel {
+  case $facts['kernel'] {
     'Linux':   {  $distell_agent_location = '/usr/local/bin/distelli-download' }
     'windows': {  $distell_agent_location = 'C:/Program Files/Distelli/distelli-download.ps1' }
     default:   { fail('Unsupported OS') }
--- site/profile/manifests/puppet/cd4pe_buildserver.pp
+++ site/profile/manifests/puppet/cd4pe_buildserver.pp
@@ -1,5 +1,5 @@
 class profile::puppet::cd4pe_buildserver {
-  case $::kernel {
+  case $facts['kernel'] {
     'Linux':   {  include ::profile::puppet::cd4pe_buildserver::linux }
     'windows': {  include ::profile::puppet::cd4pe_buildserver::windows }
     default:   { fail('Unsupported OS') }
