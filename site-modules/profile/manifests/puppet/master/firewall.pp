5c824be6a880c7ba84f1bb8cbc399850c5ffe299
Administrator <admin@example.com>
2019-03-26 22:13:56 +0000

    Add port 8000 for CD4PE

--- site-modules/profile/manifests/puppet/master/firewall.pp
+++ site-modules/profile/manifests/puppet/master/firewall.pp
@@ -9,6 +9,7 @@ class profile::puppet::master::firewall {
 
   firewall { '110 allow PE Console access':           dport => '443';   }
   firewall { '110 allow PE Services API access':      dport => '4433';  }
+  firewall { '110 allow CD4PE push access':           dport => '8000';  }
   firewall { '110 allow PuppetDB access':             dport => '8081';  }
   firewall { '110 allow Puppet Server access':        dport => '8140';  }
   firewall { '110 allow Puppet PCP Broker access':    dport => '8142';  }
