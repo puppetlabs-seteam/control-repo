ccd809c09453c0fc48ba4ad8b16911a9b66edab0
Reid Vandewiele <reid@puppetlabs.com>
2019-05-16 15:16:07 -0700

    Update config_version scripts
    Use the latest content from the reference repository,
    puppetlabs/control-repo.

--- scripts/config_version.rb
+++ scripts/config_version.rb
@@ -1,23 +1,25 @@
+#!/opt/puppetlabs/puppet/bin/ruby
 begin
   require 'rugged'
-rescue LoadError => e
+  require 'socket'
+rescue LoadError
   t = Time.new
   puts t.to_i
 else
-
   environmentpath = ARGV[0]
   environment     = ARGV[1]
 
+  # Get the hostname of the Puppet master compiling the catalog.
+  # Sometimes the hostname is the fqdn, so we'll take the first segment.
+  compiling_master = Socket.gethostname.split('.').first
+
+  # Get the path to the environment being compiled.
   repo = Rugged::Repository.discover(File.join(environmentpath, environment))
-  head  = repo.head
+  head = repo.head
 
-  #sha1 hash of the newest commit
-  head_sha = head.target_id
+  # First 12 characters of the sha1 hash of the newest commit.
+  commit_id = head.target_id[0...11]
 
-  #the commit message associated the newest commit
-  commit = repo.lookup(head_sha)
-
-  #add something to find the remote url
-
-  puts head_sha 
+  # Show the compiling master, environment name, and commit ID.
+  puts "#{compiling_master}-#{environment}-#{commit_id}"
 end
