a2680b201be15e94b12227228c6e45f0e2864ad4
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2019-05-31 12:38:19 +0200

    Adopt abuxton's changes for splunk_hec.pp

--- site-modules/profile/manifests/puppet/splunk_hec.pp
+++ site-modules/profile/manifests/puppet/splunk_hec.pp
@@ -1,11 +1,50 @@
 # Class: profile::puppet::splunk_hec
+# @summary Installs  Splunk hec and configures Puppet report processor for Splunk,
 #
 #
-class profile::puppet::splunk_hec {
+# @example
+#   include profile::puppet::splunk_hec
+#
+# @param splunk_server
+#   Specifies a Splunk server if not specified used Fact[fqdn]
+#   setup to be referenced from another profile as `$profile::puppet::splunk_server_fqdn`
+#
+# @param splunk_hec_token
+#   'bba862fd-c09c-43e1-90f7-87221f362296', # must match the value for splunk_input['hec_puppetsummary_token']
+#
+class profile::puppet::splunk_hec (
+  Optional[String]  $splunk_server    = undef,
+  String            $splunk_hec_token = 'bba862fd-c09c-43e1-90f7-87221f362296', # must match the value for splunk_input['hec_puppetsummary_token']
+) {
+  case $splunk_server {
+    undef: {
+      if defined_with_params(Class[splunk::params], {'server' => $facts['fqdn'] }) {
+          $splunk_server_fqdn = $facts['fqdn']
+      }
+      else {
+        $splunk_server_query = 'nodes[certname]{
+          resources {
+            type = "Class"
+            and
+            title = "profile::infrastructure::splunk::splunk_server"
+            }
+          }'
+        $puppetdb_result = puppetdb_query($splunk_server_query).map |$value| { $value["certname"] }
+        $splunk_server_fqdn = String.new($puppetdb_result[0])
+      }
+    }
+    default: {
+      $splunk_server_fqdn = $splunk_server
+    }
+  }
+
+  if $splunk_server_fqdn != undef and $splunk_server_fqdn != '' {
+  notice("Splunk server FQDN is: ${splunk_server_fqdn}")
+
   # resources
   class {'splunk_hec':
-    server => 'splunk.inf.puppet.vm',                  # replace with your Splunk servername
-    token  => 'bba862fd-c09c-43e1-90f7-87221f362296'   # must match the value for splunk_input['hec_puppetsummary_token']
+    server => $splunk_server_fqdn,                  
+    token  => $splunk_hec_token,
   }
 
   ini_setting { '[master:reports]':
@@ -14,7 +53,7 @@ class profile::puppet::splunk_hec {
     setting           => 'reports',
     key_val_separator => '=',
     value             => 'puppetdb,splunk_hec',
-    path              => $facts['puppet_config'],
+    path              => $settings::config,
     notify            => Service['pe-puppetserver']
   }
 }
