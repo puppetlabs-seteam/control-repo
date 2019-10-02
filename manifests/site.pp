235c0848cfaa29db8be10e8c3eca625f0ab0227a
Keith Apps <github@apps.org.uk>
2019-10-02 12:33:04 +0100

    Enable package inventory collection by default.

--- manifests/site.pp
+++ manifests/site.pp
@@ -17,3 +17,15 @@ node default {
     }
   }
 }
+
+# Enable the "package inventory" function by default
+node 'puppet.classroom.puppet.com' {
+  pe_node_group { 'PE Agent':
+    classes => {
+      'puppet_enterprise::profile::agent' => {
+        'pcp_broker_host' => 'puppet.classroom.puppet.com',
+        'package_inventory_enabled' => true,
+      }
+    }
+  }
+}
