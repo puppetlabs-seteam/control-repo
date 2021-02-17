f03c70400a8a0041849f2df74c9dcfb34ecaa222
Jerry Mozes <jerry@puppet.com>
2021-02-17 11:08:25 -0700

    Add CD4PE WS manifests

--- site-modules/profile/manifests/platform/baseline/windows.pp
+++ site-modules/profile/manifests/platform/baseline/windows.pp
@@ -7,4 +7,6 @@ class profile::platform::baseline::windows {
   include ::profile::platform::baseline::windows::packages
   include ::profile::platform::baseline::users::windows
 
+  #include ::profile::platform::baseline::windows::win_corp_baseline
+
 }
