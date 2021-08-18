3e488b53cb600c4b6dd275aae1ad7f265f602c4f
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2021-08-18 09:53:05 +0200

    Update to v0.3.1 of the module

--- site-modules/deployments/lib/puppet/functions/deployments/servicenow_change_request.rb
+++ site-modules/deployments/lib/puppet/functions/deployments/servicenow_change_request.rb
@@ -5,20 +5,21 @@ require 'json'
 
 Puppet::Functions.create_function(:'deployments::servicenow_change_request') do
   dispatch :servicenow_change_request do
-    required_param 'String',  :endpoint
-    required_param 'Hash',    :proxy
-    required_param 'String',  :username
-    required_param 'String',  :password
-    required_param 'Hash',    :report
-    required_param 'String',  :ia_url
-    required_param 'String',  :promote_to_stage_name
-    required_param 'Integer', :promote_to_stage_id
-    required_param 'String',  :assignment_group
-    required_param 'String',  :connection_alias
-    required_param 'Boolean', :auto_create_ci
+    required_param 'String',    :endpoint
+    required_param 'Hash',      :proxy
+    required_param 'String',    :username
+    required_param 'Sensitive', :password
+    required_param 'Sensitive', :oauth_token
+    required_param 'Hash',      :report
+    required_param 'String',    :ia_url
+    required_param 'String',    :promote_to_stage_name
+    required_param 'Integer',   :promote_to_stage_id
+    required_param 'String',    :assignment_group
+    required_param 'String',    :connection_alias
+    required_param 'Boolean',   :auto_create_ci
   end
 
-  def servicenow_change_request(endpoint, proxy, username, password, report, ia_url, promote_to_stage_name, promote_to_stage_id, assignment_group, connection_alias, auto_create_ci)
+  def servicenow_change_request(endpoint, proxy, username, password, oauth_token, report, ia_url, promote_to_stage_name, promote_to_stage_id, assignment_group, connection_alias, auto_create_ci)
     # Map facts to populate when auto-creating CI's
     fact_map = {
       # PuppetDB fact => ServiceNow CI field
@@ -39,8 +40,8 @@ Puppet::Functions.create_function(:'deployments::servicenow_change_request') do
     description = CGI.escape(descr).gsub(%r{\+}, '%20')
     short_description = CGI.escape("Puppet Code - '#{report['scm']['description']}' to stage '#{promote_to_stage_name}'").gsub(%r{\+}, '%20')
     request_uri = "#{endpoint}/api/sn_chg_rest/v1/change/normal?category=Puppet%20Code&short_description=#{short_description}&description=#{description}"
-    request_response = make_request(request_uri, :post, proxy, username, password)
-    raise Puppet::Error, "Received unexpected response from the ServiceNow endpoint: #{request_response.code} #{request_response.body}" unless request_response.is_a?(Net::HTTPSuccess)
+    request_response = make_request(request_uri, :post, proxy, username, password, oauth_token)
+    raise Puppet::Error, "1-Received unexpected response from the ServiceNow endpoint: #{request_response.code} #{request_response.body}" unless request_response.is_a?(Net::HTTPSuccess)
 
     changereq = JSON.parse(request_response.body)
 
@@ -49,8 +50,8 @@ Puppet::Functions.create_function(:'deployments::servicenow_change_request') do
     report['notes'].each do |ia|
       ia['IA_node_reports'].each_key do |node|
         ci_req_uri = "#{endpoint}/api/now/table/cmdb_ci?sysparm_query=name=#{node}"
-        ci_req_response = make_request(ci_req_uri, :get, proxy, username, password)
-        raise Puppet::Error, "Received unexpected response from the ServiceNow endpoint: #{ci_req_response.code} #{ci_req_response.body}" unless ci_req_response.is_a?(Net::HTTPOK)
+        ci_req_response = make_request(ci_req_uri, :get, proxy, username, password, oauth_token)
+        raise Puppet::Error, "2-Received unexpected response from the ServiceNow endpoint: #{ci_req_response.code} #{ci_req_response.body}" unless ci_req_response.is_a?(Net::HTTPOK)
 
         ci = JSON.parse(ci_req_response.body)
         if ci['result'].count.positive?
@@ -87,8 +88,8 @@ Puppet::Functions.create_function(:'deployments::servicenow_change_request') do
           end
           # Make the API call
           new_ci_uri = "#{endpoint}/api/now/table/cmdb_ci_server"
-          new_ci_response = make_request(new_ci_uri, :post, proxy, username, password, fact_payload)
-          raise Puppet::Error, "Received unexpected response from the ServiceNow endpoint: #{new_ci_response.code} #{new_ci_response.body}" unless new_ci_response.is_a?(Net::HTTPSuccess)
+          new_ci_response = make_request(new_ci_uri, :post, proxy, username, password, oauth_token, fact_payload)
+          raise Puppet::Error, "3-Received unexpected response from the ServiceNow endpoint: #{new_ci_response.code} #{new_ci_response.body}" unless new_ci_response.is_a?(Net::HTTPSuccess)
 
           # Grab the response to push the sys_id to the array_of_cis
           new_ci = JSON.parse(new_ci_response.body)
@@ -104,14 +105,14 @@ Puppet::Functions.create_function(:'deployments::servicenow_change_request') do
       assoc_ci_uri = "#{endpoint}/api/sn_chg_rest/v1/change/#{changereq['result']['sys_id']['value']}/ci"
       payload = { 'cmdb_ci_sys_ids' => array_of_cis.join(','), 'association_type' => 'affected' }
       # This next request will fail on ServiceNow versions older than New York, if so we fallback to the old task_ci mechanism
-      assoc_ci_response = make_request(assoc_ci_uri, :post, proxy, username, password, payload)
+      assoc_ci_response = make_request(assoc_ci_uri, :post, proxy, username, password, oauth_token, payload)
       if assoc_ci_response.is_a?(Net::HTTPSuccess)
         # This is New York or newer, continue to process response
         assoc_ci_worker = JSON.parse(assoc_ci_response.body)
         assoc_ci_worker_uri = "#{endpoint}/api/sn_chg_rest/change/worker/#{assoc_ci_worker['result']['worker']['sysId']}"
         while assoc_ci_worker['result']['state']['value'] < 3
           sleep 3
-          assoc_ci_response = make_request(assoc_ci_worker_uri, :get, proxy, username, password)
+          assoc_ci_response = make_request(assoc_ci_worker_uri, :get, proxy, username, password, oauth_token)
           assoc_ci_worker = JSON.parse(assoc_ci_response.body)
         end
         raise Puppet::Error, "Failed to associate CI's, got these error(s): #{assoc_ci_worker['result']['messages']['errorMessages']}" unless assoc_ci_worker['result']['state']['value'] == 3
@@ -120,12 +121,12 @@ Puppet::Functions.create_function(:'deployments::servicenow_change_request') do
         array_of_cis.each do |ci|
           task_ci_uri = "#{endpoint}/api/now/table/task_ci"
           task_ci_payload = { 'ci_item' => ci, 'task' => changereq['result']['sys_id']['value'] }
-          task_ci_response = make_request(task_ci_uri, :post, proxy, username, password, task_ci_payload)
-          raise Puppet::Error, "Received unexpected response from the ServiceNow endpoint: #{task_ci_response.code} #{task_ci_response.body}" unless task_ci_response.is_a?(Net::HTTPSuccess)
+          task_ci_response = make_request(task_ci_uri, :post, proxy, username, password, oauth_token, task_ci_payload)
+          raise Puppet::Error, "4-Received unexpected response from the ServiceNow endpoint: #{task_ci_response.code} #{task_ci_response.body}" unless task_ci_response.is_a?(Net::HTTPSuccess)
         end
       else
         # A real error occurred, raise the error
-        raise Puppet::Error, "Received unexpected response from the ServiceNow endpoint: #{assoc_ci_response.code} #{assoc_ci_response.body}"
+        raise Puppet::Error, "5-Received unexpected response from the ServiceNow endpoint: #{assoc_ci_response.code} #{assoc_ci_response.body}"
       end
     end
 
@@ -152,8 +153,8 @@ Puppet::Functions.create_function(:'deployments::servicenow_change_request') do
 
     # Get sys_id of given assignment_group
     assignment_group_url = "#{endpoint}/api/now/table/sys_user_group?sysparm_query=name=#{assignment_group}"
-    assignment_group_response = make_request(assignment_group_url, :get, proxy, username, password)
-    raise Puppet::Error, "Received unexpected response from the ServiceNow endpoint: #{assignment_group_response.code} #{assignment_group_response.body}" unless assignment_group_response.is_a?(Net::HTTPOK) # rubocop:disable Metrics/LineLength
+    assignment_group_response = make_request(assignment_group_url, :get, proxy, username, password, oauth_token)
+    raise Puppet::Error, "6-Received unexpected response from the ServiceNow endpoint: #{assignment_group_response.code} #{assignment_group_response.body}" unless assignment_group_response.is_a?(Net::HTTPOK) # rubocop:disable Metrics/LineLength
 
     arr_assignment_groups = JSON.parse(assignment_group_response.body)['result']
     raise Puppet::Error, "No Assignment Group named '#{assignment_group}' was found in ServiceNow!" unless arr_assignment_groups.count.positive?
@@ -164,7 +165,7 @@ Puppet::Functions.create_function(:'deployments::servicenow_change_request') do
     change_req_url = "#{endpoint}/api/sn_chg_rest/v1/change/normal/#{changereq['result']['sys_id']['value']}?state=assess"
     payload = {}.tap do |data|
       data[:state] = 'assess'
-      data[:risk_impact_analysis] = ia_url + "\n" + report['log']
+      data[:risk_impact_analysis] = ia_url + "\n" + report['log'] # rubocop:disable Style/StringConcatenation
       data[:assignment_group] = assignment_group_sys_id
       data[:close_notes] = closenotes.to_json
       data[:priority] = 3.0                           # 1.0 = Critical / 2.0 = High / 3.0 = Moderate / 4.0 = Low
@@ -172,11 +173,11 @@ Puppet::Functions.create_function(:'deployments::servicenow_change_request') do
       data[:risk] = bln_ia_safe_verdict ? 4.0 : 2.0   # 2.0 = High / 3.0 = Medium / 4.0 = Low
     end
 
-    change_req_url_res = make_request(change_req_url, :patch, proxy, username, password, payload)
-    raise Puppet::Error, "Received unexpected response from the ServiceNow endpoint: #{change_req_url_res.code} #{change_req_url_res.body}" unless change_req_url_res.is_a?(Net::HTTPSuccess)
+    change_req_url_res = make_request(change_req_url, :patch, proxy, username, password, oauth_token, payload)
+    raise Puppet::Error, "7-Received unexpected response from the ServiceNow endpoint: #{change_req_url_res.code} #{change_req_url_res.body}" unless change_req_url_res.is_a?(Net::HTTPSuccess)
   end
 
-  def make_request(endpoint, type, proxy, username, password, payload = nil)
+  def make_request(endpoint, type, proxy, username, password, oauth_token, payload = nil)
     uri = URI.parse(endpoint)
     max_attempts = 3
     attempts = 0
@@ -198,7 +199,11 @@ Puppet::Functions.create_function(:'deployments::servicenow_change_request') do
         else
           raise Puppet::Error, "servicenow_change_request#make_request called with invalid request type #{type}"
         end
-        request.basic_auth(username, password)
+        if oauth_token.unwrap.to_s.strip.empty?
+          request.basic_auth(username, password.unwrap.delete_prefix('"').delete_suffix('"'))
+        else
+          request['Authorization'] = "Bearer #{oauth_token.unwrap.delete_prefix('"').delete_suffix('"')}"
+        end
         request['Content-Type'] = 'application/json'
         request['Accept'] = 'application/json'
         if proxy['enabled'] == true
@@ -206,7 +211,7 @@ Puppet::Functions.create_function(:'deployments::servicenow_change_request') do
             proxy['host'],
             proxy['port'],
           )
-          response = proxy_conn.start(uri.host, uri.port, :use_ssl => (uri.scheme == 'https')) do |http| # rubocop:disable Style/HashSyntax
+          response = proxy_conn.start(uri.host, uri.port, :use_ssl => (uri.scheme == 'https')) do |http|
             http.read_timeout = 60
             http.request(request)
           end
