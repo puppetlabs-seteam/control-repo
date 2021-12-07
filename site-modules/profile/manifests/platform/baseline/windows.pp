dcef06cace87ddb92f03f9e218ad586cbee50f9e
Jerry Mozes <jerry@puppet.com>
2021-12-07 12:04:30 -0700

    Comment out the bootstrap include for cd4pe ws

--- site-modules/profile/manifests/platform/baseline/windows.pp
+++ site-modules/profile/manifests/platform/baseline/windows.pp
@@ -1,6 +1,6 @@
 class profile::platform::baseline::windows {
 
-  include ::profile::platform::baseline::windows::bootstrap
+  # include ::profile::platform::baseline::windows::bootstrap
   include ::profile::platform::baseline::windows::common
   include ::profile::platform::baseline::windows::motd
   include ::profile::platform::baseline::windows::firewall
