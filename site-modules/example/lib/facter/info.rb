429244317090e19fbdb706f6e9af7cdc2ecdc861
halimwi <hwijaya@gmail.com>
2022-09-26 18:21:32 +0800

    Update info.rb

--- site-modules/example/lib/facter/info.rb
+++ site-modules/example/lib/facter/info.rb
@@ -1,6 +1,4 @@
 Facter.add(:info) do
-  confine :osfamily => 'RedHat'
-  confine :operatingsystemmajrelease => '7'
   setcode do
     Facter::Core::Execution.exec('cat /etc/puppetlabs/puppet/info.txt')
   end
