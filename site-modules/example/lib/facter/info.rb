d1dc9ecda5514b8e3579e56d807d825dcf4ca067
halimwi <hwijaya@gmail.com>
2022-10-06 15:39:19 +0800

    Update info.rb

--- site-modules/example/lib/facter/info.rb
+++ site-modules/example/lib/facter/info.rb
@@ -5,7 +5,7 @@ Facter.add(:info) do
     when 'Linux'
       Facter::Core::Execution.exec('cat /etc/puppetlabs/puppet/info.txt')
     when 'windows'
-      Facter::Core::Execution.exec('type C:\ProgramData\PuppetLabs\puppet\info.txt')
+      Facter::Core::Execution.exec('type C:/ProgramData/PuppetLabs/puppet/info.txt')
     else
       'production'
     end  
