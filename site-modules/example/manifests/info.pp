ecd72b988ef455724c9410a89294358873e39009
halimwi <hwijaya@gmail.com>
2022-10-06 15:21:24 +0800

    Update info.pp

--- site-modules/example/manifests/info.pp
+++ site-modules/example/manifests/info.pp
@@ -1,7 +1,24 @@
 class example::info {
-  file { '/etc/puppetlabs/puppet/info.txt':
-    ensure  => 'present',
-    content => "production",
-    mode    => '0644',
+
+  # OS Specific
+  case $::kernel {
+    'windows': {
+       file { '/etc/puppetlabs/puppet/info.txt':
+         ensure  => 'present',
+         content => "production",
+         mode    => '0644',
+       }
+    }
+    'Linux':   {
+       file { 'C:\ProgramData\PuppetLabs\puppet\info.txt':
+         ensure  => 'present',
+         content => "production",
+         mode    => '0644',
+       }
+    }
+    default: {
+      fail('Unsupported operating system!')
+    }
   }
+  
 }
