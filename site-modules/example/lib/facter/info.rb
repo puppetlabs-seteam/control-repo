5883b56fc023975ffdc8f017eadcf4fa8a58bc46
halimwi <hwijaya@gmail.com>
2022-10-06 15:05:43 +0800

    Update info.rb

--- site-modules/example/lib/facter/info.rb
+++ site-modules/example/lib/facter/info.rb
@@ -1,5 +1,11 @@
 Facter.add(:info) do
   setcode do
-    Facter::Core::Execution.exec('cat /etc/puppetlabs/puppet/info.txt')
+    kernel = Facter.value(:kernel)
+    case kernel
+    when 'Linux'
+      Facter::Core::Execution.exec('cat /etc/puppetlabs/puppet/info.txt')
+    else
+      Facter::Core::Execution.exec('type C:\ProgramData\PuppetLabs\puppet\info.txt')
+    end  
   end
 end
