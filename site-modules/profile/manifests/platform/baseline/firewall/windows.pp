2a9144cd5bd06e49b3d7b06b720ad4e8dd21c17b
Paul Reed <paul.reed@puppet.com>
2023-11-14 21:11:22 -0500

    update windows to use webalex-windows_firewall

--- site-modules/profile/manifests/platform/baseline/firewall/windows.pp
+++ site-modules/profile/manifests/platform/baseline/firewall/windows.pp
@@ -13,36 +13,39 @@ class profile::platform::baseline::firewall::windows (
   Boolean $allow_ingress_icmpv4 = $profile::platform::baseline::firewall::allow_ingress_icmpv4,
   Array[Hash] $allow_ingress    = $profile::platform::baseline::firewall::allow_ingress_windows_default + $profile::platform::baseline::firewall::allow_ingress, #lint:ignore:140chars
 ) {
-  # Make sure the service is up and running
-  class { 'windows_firewall': ensure => 'running', }
+  # Purge all unmanaged rules
+  resources { 'windows_firewall_rule': purge => true, }
 
   # Set the default outbound action
-  $default_outbound_action = $allow_egress ? { true => 'allow', false => 'deny' }
-  windowsfirewall { ['domain','public','private']:
-    ensure                  => present,
-    default_outbound_action => $default_outbound_action,
+  $outbound_policy = $allow_egress ? { true => 'allowoutbound', false => 'blockoutbound' }
+  windows_firewall_profile { ['domain','public','private']:
+    inboundusernotification    => 'disable',
+    firewallpolicy             => "blockinbound,${outbound_policy}",
+    remotemanagement           => 'disable',
+    state                      => true,
+    unicastresponsetomulticast => 'disable',
   }
 
   if ($allow_ingress_icmpv4) {
-    windows_firewall::exception { 'Permit ICMPv4':
+    windows_firewall_rule { 'Permit ICMPv4':
       ensure       => present,
-      direction    => 'in',
+      direction    => 'inbound',
       action       => 'allow',
       enabled      => true,
-      protocol     => 'ICMPv4',
+      protocol     => 'icmpv4',
       display_name => 'Permit ICMPv4',
       description  => 'Permit ICMPv4',
     }
   }
 
   $allow_ingress.each | Hash $in | {
-    windows_firewall::exception { $in['name']:
+    windows_firewall_rule { $in['name']:
       ensure       => present,
-      direction    => 'in',
+      direction    => 'inbound',
       action       => 'allow',
       enabled      => true,
-      protocol     => $in['protocol'].upcase(),
-      local_port   => $in['port'],
+      protocol     => $in['protocol'].downcase(),
+      local_port   => String($in['port']),
       remote_port  => 'any',
       display_name => "${in['name']} - Inbound - Puppet Managed",
       description  => "[${in['protocol'].upcase()}-${in['port']}] ${in['name']} ${in['description']} - Puppet Managed",
