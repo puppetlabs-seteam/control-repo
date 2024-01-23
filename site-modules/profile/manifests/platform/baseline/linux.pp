99a29f9f7fedd6daa58fe2769b560cb87d733cb1
Paul Reed <paul.reed@puppet.com>
2024-01-23 14:42:47 -0500

    Disable and remove SSH Server from Linux nodes.

--- site-modules/profile/manifests/platform/baseline/linux.pp
+++ site-modules/profile/manifests/platform/baseline/linux.pp
@@ -5,9 +5,9 @@ class profile::platform::baseline::linux {
   include profile::platform::baseline::linux::packages
   include profile::platform::baseline::linux::vim
   include profile::platform::baseline::linux::users
+  include profile::platform::baseline::linux::ssh
 
   if !defined(Class['Cem_linux']) {
     include profile::platform::baseline::linux::motd
-    include profile::platform::baseline::linux::ssh
   }
 }
--- site-modules/profile/manifests/platform/baseline/linux/ssh.pp
+++ site-modules/profile/manifests/platform/baseline/linux/ssh.pp
@@ -1,8 +1,18 @@
 #
 class profile::platform::baseline::linux::ssh () {
-  if !defined(Class['ssh']) and !defined(Class['cem_linux']) {
-    class { 'ssh':
-      purge_keys => false,
+  # Disable and remove SSH Server to reduce attack vectors.
+  # We don't need SSH server services when managing with Puppet.
+  case $facts['os']['name'] {
+    'CentOS': {
+      service { 'sshd': ensure => stopped, enable => false, }
+      -> package { 'OpenLogicEnv': ensure => absent, }
+      -> package { 'azure-repo-svc': ensure => absent, }
+      -> package { 'WALinuxAgent': ensure => absent, }
+      -> package { 'openssh-server': ensure => absent, }
+    }
+    default: {
+      service { 'sshd': ensure => stopped, enable => false, }
+      -> package { 'openssh-server': ensure => absent, }
     }
   }
 }
