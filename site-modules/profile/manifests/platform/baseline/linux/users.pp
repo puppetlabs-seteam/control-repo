55b8aa26630ad8b482092ddd1c7a50aa035072fd
Paul Reed <paul.reed@puppet.com>
2024-01-24 16:08:27 -0500

    Changes to baseline for stricter sudo rules and puppetadmin user

--- site-modules/profile/manifests/platform/baseline/linux/users.pp
+++ site-modules/profile/manifests/platform/baseline/linux/users.pp
@@ -1,7 +1,20 @@
+# @summary 
+# Define default users on linux systems
 #
+# @param password
+# Default password for puppet user accounts.
+# @param puppetadmin_password
 class profile::platform::baseline::linux::users (
-  $password, # This should be a password hash for use in /etc/shadow.
+  String $password, # This should be a password hash for use in /etc/shadow.
+  String $puppetadmin_password,
 ) {
+  user { 'puppetadmin':
+    ensure   => 'present',
+    comment  => 'Admin User Account',
+    home     => '/home/puppetadmin',
+    password => $puppetadmin_password,
+    shell    => '/bin/bash',
+  }
   user { 'PuppetSE':
     ensure   => 'present',
     comment  => 'SE Demo Account',
