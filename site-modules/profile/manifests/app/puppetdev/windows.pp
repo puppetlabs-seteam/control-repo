7bef96cf1e9c20c41f7b0ea8893bb5fba80cd99b
Stephen P. Potter <steve.potter@perforce.com>
2024-08-05 17:19:23 -0400

    removing the puppetdev profile

--- site-modules/profile/manifests/app/puppetdev/windows.pp
+++
@@ -1,7 +0,0 @@
-class profile::app::puppetdev::windows {
-
-  ensure_packages(['git', 'pdk'], { ensure => present, provider => 'chocolatey' })
-
-  windows_env { 'PATH=C:\Program Files\Puppet Labs\DevelopmentKit\private\ruby\2.1.9\bin': require => Package['pdk'] }
-
-}
